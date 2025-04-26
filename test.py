f370df46fa027e320e6a036843b3e91dd3b09e49
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2025-04-26 17:39:24 +0530

    testing modules

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
-#from rush.csrc import cuda_core, cpu_core
 from rush.engine import cpu_core, cuda_core
 from rush import dtypes
 
-#x = [1,2,3,4,5,6]
-#device_ptr = cuda_core.toCUDA(x, dtypes.ulonglong.fmt)
-#print(device_ptr)
-#print(cuda_core.toList(device_ptr, len(x), dtypes.ulonglong.fmt))
-#host_ptr = cuda_core.toCPU(device_ptr, len(x), dtypes.ulonglong.fmt)
-#print(cpu_core.toList(host_ptr, len(x), dtypes.ulonglong.fmt))
 
 x = 23
 dptr = cuda_core.s_toCUDA(x, dtypes.uint8.fmt)
