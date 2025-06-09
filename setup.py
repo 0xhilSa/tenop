d978a313cdf33d87b6fd42b92d15264095e307fa
Sahil Rajwar <sahilrajwar2004@gmail.com>
2025-06-10 02:07:07 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = "cuten",
+  name = "tenop",
   version = "0.0.1",
   author = "Sahil Rajwar",
   license = "MIT",
@@ -9,7 +9,7 @@ setup(
   long_description=open("README.md").read(),
   long_description_content_type="text/markdown",
   packages=find_packages(),
-  package_data={"cuten.engine": ["*.so", "*.pyi"]},
+  package_data={"tenop.engine": ["*.so", "*.pyi"]},
   classifiers=[
     "Programming Language :: Python :: 3",
     "Operating System :: POSIX :: Linux",
