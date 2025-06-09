9bda345fb63041f369fabb5e4df4a72b1d499987
Sahil Rajwar <sahilrajwar2004@gmail.com>
2025-06-10 01:39:52 +0530

    get device name CUDA or CPU

--- cuten/device.py
+++ cuten/device.py
@@ -1,4 +1,4 @@
-from .engine import cuda
+from .engine import cuda, cpu
 
 DEVICES = ["cpu", "cuda"]
 
@@ -9,15 +9,19 @@ class Device:
     self.__type = parts[0]
     self.__index = 0
     if len(parts) == 2:
-        try: self.__index = abs(int(parts[1]))
-        except ValueError: raise ValueError(f"Invalid device index in '{device}'")
+      try: self.__index = abs(int(parts[1]))
+      except ValueError: raise ValueError(f"Invalid device index in '{device}'")
     if self.__type == "cpu":
-        if self.__index != 0: raise ValueError("CPU does not support indexing other than 0")
+      if self.__index != 0: raise ValueError("CPU does not support indexing other than 0")
     elif self.__type == "cuda":
-        available = cuda.count()
-        if abs(self.__index) >= available: raise RuntimeError(f"CUDA device index {self.__index} exceeds available devices ({available})")
+      available = cuda.count()
+      if abs(self.__index) >= available: raise RuntimeError(f"CUDA device index {self.__index} exceeds available devices ({available})")
   def __repr__(self): return f"<Device(device='{self.__type}', index={self.__index})>"
   @property
   def type_(self): return self.__type
   @property
   def index(self): return self.__index
+  @property
+  def name(self):
+    if self.__type == "cuda": return cuda.get_device_name(self.__index)
+    elif self.__type == "cpu": return cpu.get_device_name()[2:]
